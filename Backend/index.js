0ded13aa3d631b7544b6b2e11f53e3c6608d47b8
yogesh Budha <yogeshbudha829@gmail.com>
2024-11-23 14:35:59 +0545

    adding the cors

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,10 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors())
+app.use(cors({
+    origin: "*", 
+    credentials: true
+}))
 app.use("/uploads",express.static("./public/uploads"))
 
 
